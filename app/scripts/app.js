eaac6ba554aa77aef73c1821f518a0f9a7220159
Mukesh Mehra <Mukesh Mehra>
2016-04-16 19:39:10 +0530

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name htmlGamingApp
+ * @description
+ * # htmlGamingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('htmlGamingApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name htmlGamingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the htmlGamingApp
+ */
+angular.module('htmlGamingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
